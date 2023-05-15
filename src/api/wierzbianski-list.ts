ef9aee7203373bbbcd352b69446ea171ea3f40e4
afernandez <afernandez@bspot.com>
2023-05-15 09:39:38 +0100

    mongodb async

--- src/api/wierzbianski-list.ts
+++ src/api/wierzbianski-list.ts
@@ -2,35 +2,21 @@ import type { RequestHandler } from "express";
 import { connectToDatabase } from "../common/mongo-db";
 
 
+const resolveData = async () => {
+    const { database } = await connectToDatabase()
+    const collection = database.collection('contents');
+    const content = await collection.find({}).toArray();
+    return content;
+}
+
 export const getWierzbianskiContentList: RequestHandler = (req, res, next) => {
 
-
     console.log(' ==> getWierzbianskiContentList');
-
-    connectToDatabase().then(({ database } ) => {
-
-        console.log(' ==> connected to Mongo');
-
-        const collection = database.collection('contents');
-        collection.find({}).then((content) => {
-            console.log(' ==>  collection find content');
-            res.status(200).json(content);
-        })
-        .catch((err) => {
-            console.log(' ==> error collection find content');
-            console.log(err);
-            return res.status(422).send({message: err});
-        });
+    resolveData().then((content) => {
+        console.log(' ==>  collection find content');
+        res.status(200).json(content);
     })
     .catch((err) => {
-        console.log(' ==> error connecting to Mongo');
-        console.log(err);
         return res.status(422).send({message: err});
     });
-
-
-
-
-
-
 };
