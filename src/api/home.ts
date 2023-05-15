99afe9a5a5ee14d5733cc19150d3d8b28e2e7508
afernandez <afernandez@bspot.com>
2023-05-15 10:28:38 +0100

    Remove home

--- src/api/home.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestHandler } from "express";
-
-export const home: RequestHandler = (req, res, next) => {
-  res.status(200).json({
-    site: "https://api.freepowder.io/",
-    version: "1.0",
-    description: "serverless express api",
-  });
-};
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,3 @@ export * from "./sign-up";
 export * from "./sign-out";
 export * from "./wierzbianski-list";
 export * from "./wierzbianski-item";
-export * from "./home";
