5cfe383ff00f81763df203273e2f733603201bab
afernandez <afernandez@bspot.com>
2023-05-15 11:25:45 +0100

    right collection

--- src/common/auth.ts
+++ src/common/auth.ts
@@ -4,7 +4,7 @@ import { connectToDatabase } from "../common/mongo-db";
 
 const resolveUser = async (email) => {
     const { database } = await connectToDatabase();
-    const collection = database.collection("contents");
+    const collection = database.collection("users");
     const user = await collection.findOne({ email: email });
     console.log('resolveUser ==> ', user);
     return user;
