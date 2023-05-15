b67098e95679834fb4d987ff0d42990e0a731fd7
afernandez <afernandez@bspot.com>
2023-05-15 09:27:55 +0100

    Mongo 422

--- src/api/wierzbianski-list.ts
+++ src/api/wierzbianski-list.ts
@@ -4,16 +4,27 @@ import { connectToDatabase } from "../common/mongo-db";
 
 export const getWierzbianskiContentList: RequestHandler = (req, res, next) => {
 
+
+    console.log(' ==> getWierzbianskiContentList');
+
     connectToDatabase().then(({ database } ) => {
+
+        console.log(' ==> connected to Mongo');
+
         const collection = database.collection('contents');
         collection.find({}).then((content) => {
+            console.log(' ==>  collection find content');
             res.status(200).json(content);
         })
         .catch((err) => {
+            console.log(' ==> error collection find content');
+            console.log(err);
             return res.status(422).send({message: err});
         });
     })
     .catch((err) => {
+        console.log(' ==> error connecting to Mongo');
+        console.log(err);
         return res.status(422).send({message: err});
     });
 
